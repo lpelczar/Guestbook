6653616f2b21760c715890a3e3016399d7a25a5f
lpelczar <lukepelczar@gmail.com>
2018-04-12 15:53:45 +0200

    Implement serving static files

--- src/main/java/com/codecool/guestbook/Guestbook.java
+++ src/main/java/com/codecool/guestbook/Guestbook.java
@@ -1,46 +1,76 @@
 package com.codecool.guestbook;
 
+import com.codecool.guestbook.utils.MimeTypeResolver;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.Map;
+import java.net.URI;
+import java.net.URL;
 
 public class Guestbook implements HttpHandler {
-
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        String response = "";
-        String method = httpExchange.getRequestMethod();
+        // get file path from url
+        URI uri = httpExchange.getRequestURI();
+        System.out.println("looking for: " + uri.getPath());
+        String path = "." + uri.getPath();
 
-        if (method.equals("GET")) {
-            System.out.println("BLABLA");
+        // get file from resources folder, see: https://www.mkyong.com/java/java-read-a-file-from-resources-folder/
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL fileURL = classLoader.getResource(path);
+
+        OutputStream os = httpExchange.getResponseBody();
+
+        if (fileURL == null) {
+            // Object does not exist or is not a file: reject with 404 error.
+            send404(httpExchange);
+        } else {
+            // Object exists and is a file: accept with response code 200.
+            sendFile(httpExchange, fileURL);
         }
 
-//        // If the form was submitted, retrieve it's content.
-//        if(method.equals("POST")){
-//            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
-//            BufferedReader br = new BufferedReader(isr);
-//            String formData = br.readLine();
-//
-//            System.out.println(formData);
-//            Map inputs = parseFormData(formData);
-//
-//            response = "<html><body>" +
-//                    "<h1>Hello " +
-//                    inputs.get("firstname") + " " + inputs.get("lastname") +
-//                    "!</h1>" +
-//                    "</body><html>";
-//        }
-//
-//        httpExchange.sendResponseHeaders(200, response.length());
-//        OutputStream os = httpExchange.getResponseBody();
-//        os.write(response.getBytes());
-//        os.close();
     }
+
+    private void send404(HttpExchange httpExchange) throws IOException {
+        String response = "404 (Not Found)\n";
+        httpExchange.sendResponseHeaders(404, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.toString().getBytes());
+        os.close();
+    }
+
+    private void sendFile(HttpExchange httpExchange, URL fileURL) throws IOException {
+        // get the file
+        File file = new File(fileURL.getFile());
+
+        System.out.println(file.getAbsolutePath());
+
+        // we need to find out the mime type of the file, see: https://en.wikipedia.org/wiki/Media_type
+        MimeTypeResolver resolver = new MimeTypeResolver(file);
+        String mime = resolver.getMimeType();
+
+        System.out.println(mime);
+
+        httpExchange.getResponseHeaders().set("Content-Type", mime);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        OutputStream os = httpExchange.getResponseBody();
+
+        // send the file
+        FileInputStream fs = new FileInputStream(file);
+        final byte[] buffer = new byte[0x10000];
+        int count = 0;
+        while ((count = fs.read(buffer)) >= 0) {
+            os.write(buffer,0,count);
+        }
+        os.close();
+    }
+
+
 }
